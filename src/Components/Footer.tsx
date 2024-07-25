fd15c7d14252fade6f900904b6e35220298810f5
callmesidhu <sidharthpunalur@gmail.com>
2024-07-25 23:04:49 +0530

    basic assests added

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Footer() {
+  return (
+    <>
+    <div>
+          <h1>
+                    
+          </h1>
+    </div>
+    </>
+  )
+}
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+import Navbar from '../Components/Navbar';
 import { NotFound } from '../Pages/404Error';
 import { Home } from '../Pages/Home'
 import { Teams } from '../Pages/Teams'
@@ -6,13 +7,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 export const App = ()=>{
   
   return (
-    <Router>
-    <Routes>
+    <div className='app'>
+      <Router>
+      <Navbar/>
+      <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/teams" element={<Teams />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   </Router>
+    </div>
   )
 }
 
