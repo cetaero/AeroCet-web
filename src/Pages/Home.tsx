d5998607f7dded7a7aba5e2a57e65638821d0a24
callmesidhu <sidharthpunalur@gmail.com>
2024-08-15 22:15:38 +0530

    Horizontal scroll added

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,31 @@
-
+import React, { useEffect, useRef } from 'react';
 import './Home.css';
 
-export default function Home() {
-  return (
+const Home: React.FC = () => {
+  const scrollContainerRef = useRef<HTMLDivElement | null>(null);
 
-    <div className='home'>
+  useEffect(() => {
+    const element = scrollContainerRef.current;
+
+    if (element) {
+      const handleScroll = (event: WheelEvent) => {
+        event.preventDefault();
+        element.scrollBy({
+          left: event.deltaY < 0 ? -1500 : 1500,
+          behavior: 'smooth',
+        });
+      };
+
+      element.addEventListener('wheel', handleScroll);
+
+      return () => {
+        element.removeEventListener('wheel', handleScroll);
+      };
+    }
+  }, []);
+
+  return (
+    <div className='home' id='home' ref={scrollContainerRef}>
       <div className='main bg-orange-600'>
         <h1>AeroCET Intro</h1>
       </div>
@@ -20,8 +41,8 @@ export default function Home() {
       <div className='main bg-teal-700'>
         <h1>AeroCET Contact</h1>
       </div>
-      
     </div>
-   
   );
-}
+};
+
+export default Home;
