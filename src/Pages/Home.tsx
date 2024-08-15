cd19933e32f665420bc76cac9f6a18f34ee160df
callmesidhu <sidharthpunalur@gmail.com>
2024-08-15 21:26:33 +0530

    H-scroll added without scroll bar

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,27 +1,27 @@
-import HorizontalScroll from 'react-scroll-horizontal'
-import './Home.css'
+
+import './Home.css';
 
 export default function Home() {
-    
   return (
-    <div className=''>
 
-            <div className='main bg-orange-600'>
-                <h1>AeroCET Intro</h1>
-            </div>
-            <div className='main bg-green-600'>
-                <h1>AeroCET Gallery</h1>
-            </div>
-            <div className='main bg-rose-800'>
-                <h1>AeroCET Teams</h1>
-            </div>
-            <div className='main bg-indigo-600'>
-                <h1>AeroCET About</h1>
-            </div>
-            <div className='main bg-teal-700'>
-                <h1>AeroCET Contact</h1>
-            </div>
-       
+    <div className='home'>
+      <div className='main bg-orange-600'>
+        <h1>AeroCET Intro</h1>
+      </div>
+      <div className='main bg-green-600'>
+        <h1>AeroCET Gallery</h1>
+      </div>
+      <div className='main bg-rose-800'>
+        <h1>AeroCET Teams</h1>
+      </div>
+      <div className='main bg-indigo-600'>
+        <h1>AeroCET About</h1>
+      </div>
+      <div className='main bg-teal-700'>
+        <h1>AeroCET Contact</h1>
+      </div>
+      
     </div>
-  )
+   
+  );
 }
