3512488868ac9103e26c1a83959d4de8ef41ab8c
callmesidhu <sidharthpunalur@gmail.com>
2024-08-17 08:56:42 +0530

    before intro added

--- src/Components/Intro.tsx
+++ src/Components/Intro.tsx
@@ -1,10 +1,23 @@
-import './Intro.css'
-import plane from '../assets/images/IntroImg.png'
+import './Intro.css';
+import text from '../assets/images/introText.png';
+import plane from '../assets/images/plane.png';
+
 export default function Intro() {
   return (
-  <div className="flex w-full h-full flex-1 justify-center items-center divbg bottom-0">
-    <img src={plane} alt='Loader-image' className='' />
-  </div>
-  )
+    <div className="flex w-full h-screen flex-1 flex-col justify-center items-center divbg ">
+      {/* Plane Image with Parallax Effect */}
+      <img
+        src={plane}
+        alt="Plane"
+        className="parallax-image lg:h-94 aspect-auto lg:mr-64"
+      />
+
+      {/* Intro Text Image */}
+      <img
+        src={text}
+        alt="Intro Text"
+        className="lg:h-[36rem] aspect-auto"
+      />
+    </div>
+  );
 }
-8
