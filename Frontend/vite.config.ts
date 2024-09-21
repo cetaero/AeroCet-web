bdc26a0c8fb82d7868f2867267be58709f273b1c
callmesidhu <sidharthpunalur@gmail.com>
2024-09-21 12:31:29 +0530

    nav bar bug fixed

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // Allows network access
+    port: 3000, // You can specify any available port
+  },
   plugins: [react()],
 })
