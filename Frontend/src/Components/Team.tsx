cd692fc9698dabcf7539c2dec68b9b11f61a34e6
callmesidhu <sidharthpunalur@gmail.com>
2024-09-21 13:54:30 +0530

    Deploy bug fixed

--- Frontend/src/Components/Team.tsx
+++ Frontend/src/Components/Team.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import  { useEffect, useRef, useState } from 'react';
 import './Team.css';
 import plane from '../assets/images/vectorPlane.png';
 import drone from '../assets/images/vectorDrone.png';
